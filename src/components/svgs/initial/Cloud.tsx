dbc468f78e31489ed5405a2240648bc7238953f5
Amir Azizafshari <a_amirhossein@icloud.com>
2022-11-19 15:28:31 -0500

    adds form content and cloud svg

---
+++ src/components/svgs/initial/Cloud.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Cloud() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="243"
+      height="133"
+      fill="none"
+      className="w-full h-full object-contain"
+      viewBox="0 0 243 133"
+    >
+      <path
+        fill="#fff"
+        d="M188.556 58.513C180.595 25.09 145.118 0 102.5 0 68.663 0 39.275 15.887 24.64 39.138-10.602 42.238-38 66.94-38 96.875-38 128.941-6.505 155 32.25 155h152.208C216.773 155 243 133.3 243 106.562c0-25.575-24.002-46.306-54.444-48.05z"
+      ></path>
+    </svg>
+  );
+}
+
+export default Cloud;
