359d5113a7b60df2d9cc709d9ed616fa13bb43c8
Amir Azizafshari <a_amirhossein@icloud.com>
2022-11-19 14:58:29 -0500

    adds base styles and bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ShoppingCart from "./components/svgs/initial/ShoppingCart";
+import Title from "./components/svgs/initial/TextBehind";
 
 function App() {
-  
   return (
-    <div className="App">
-      Hello World
+    <div className="App w-screen h-screen">
+      {/* Title */}
+      <div className="absolute left-[10%] top-[10%] w-1/2">
+        <Title />
+      </div>
+      {/* Shopping Cart */}
+      <div className="absolute bottom-[10%] right-[10%] w-1/4">
+        <ShoppingCart />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
