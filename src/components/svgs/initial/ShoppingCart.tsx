99b33f3801dd187c576d279a094ab3f3013239c0
Amir Azizafshari <a_amirhossein@icloud.com>
2022-11-19 14:58:11 -0500

    adds shopping cart component

---
+++ src/components/svgs/initial/ShoppingCart.tsx
@@ -0,0 +1,81 @@
+function ShoppingCart() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="143"
+      height="154"
+      className="object-cover w-full h-full"
+      fill="none"
+      viewBox="0 0 143 154"
+    >
+      <circle cx="24.406" cy="142.83" r="10.781" fill="#000"></circle>
+      <circle cx="99.443" cy="142.83" r="10.781" fill="#000"></circle>
+      <circle cx="24.406" cy="142.83" r="9.919" fill="#fff"></circle>
+      <circle cx="99.443" cy="142.83" r="9.919" fill="#fff"></circle>
+      <path
+        stroke="#BFBFBF"
+        strokeWidth="2"
+        d="M27.562 44.937L27.562 101.862"
+      ></path>
+      <path
+        stroke="#BFBFBF"
+        strokeWidth="2"
+        d="M114.537 61.462L11.037 61.462"
+      ></path>
+      <path
+        stroke="#BFBFBF"
+        strokeWidth="2"
+        d="M107.637 91.08H20.094M111.518 76.418H15.35"
+      ></path>
+      <path
+        stroke="#BFBFBF"
+        strokeWidth="2"
+        d="M105.187 44.937L105.187 101.862"
+      ></path>
+      <path
+        stroke="#BFBFBF"
+        strokeWidth="2"
+        d="M87.074 44.937L87.074 101.862"
+      ></path>
+      <path
+        stroke="#BFBFBF"
+        strokeWidth="2"
+        d="M66.374 44.937L66.374 101.862"
+      ></path>
+      <path
+        stroke="#BFBFBF"
+        strokeWidth="2"
+        d="M46.537 44.937L46.537 101.862"
+      ></path>
+      <circle
+        cx="24.406"
+        cy="142.83"
+        r="7.331"
+        fill="#000"
+        fillOpacity="0.75"
+      ></circle>
+      <circle
+        cx="99.443"
+        cy="142.83"
+        r="7.331"
+        fill="#000"
+        fillOpacity="0.75"
+      ></circle>
+      <path
+        stroke="#FFD15B"
+        strokeWidth="6"
+        d="M123.593 6.988s14.662 5.174 13.368 11.643c-1.293 6.469-11.212.431-13.368 4.744-2.156 4.312-5.26 21.993-5.26 21.993m-13.715 57.356h-79.35L5 45.368h113.333m-13.715 57.356l13.715-57.356m-13.715 57.356s13.715.863 13.715 12.506c0 11.644-13.715 11.644-13.715 11.644h-10.35M5 137.655s3.45-10.781 6.469-10.781h8.625m0 0l5.175 16.387m-5.175-16.387h74.174m0 0l5.606 16.387"
+      ></path>
+      <rect
+        width="23.545"
+        height="9.6"
+        x="123.496"
+        fill="#E6AB13"
+        rx="4.8"
+        transform="rotate(36.03 123.496 0)"
+      ></rect>
+    </svg>
+  );
+}
+
+export default ShoppingCart;
