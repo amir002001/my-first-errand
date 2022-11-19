a7354ebe2a622545337ef2b6df14400ad736db42
Amir Azizafshari <a_amirhossein@icloud.com>
2022-11-19 16:39:21 -0500

    Changes names of components

--- src/pages/Paths.tsx
+++ src/pages/Paths.tsx
@@ -1,22 +1,33 @@
-import BuySomething from "../components/svgs/Paths/BuySomething";
-import FindParents from "../components/svgs/Paths/FindParents";
-import FindWay from "../components/svgs/Paths/FindWay";
-import StrangerDanger from "../components/svgs/Paths/StrangerDanger";
+import BuySomething from "../components/svgs/Paths/BuySomethingCloud";
+import FindParents from "../components/svgs/Paths/FindParentsCloud";
+import FindWay from "../components/svgs/Paths/FindWayCloud";
+import StrangerDanger from "../components/svgs/Paths/StrangerDangerCloud";
 import Cloud from "../components/svgs/Root/Cloud";
 
 function Paths() {
   return (
     <div className="w-screen h-screen">
-      <div className="absolute w-1/3 left-[5%] bottom-[10%]">
+      {/* clouds */}
+      <div className="absolute w-1/3 opacity-[38%] top-0 -z-20 -translate-x-1/3 -translate-y-1/3">
+        <Cloud />
+      </div>
+      <div className="absolute w-1/3 opacity-[38%] bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 -z-20 ">
+        <Cloud />
+      </div>
+      <div className="absolute w-1/3 opacity-[38%] right-0 top-[5%] -z-20 translate-x-1/3">
+        <Cloud />
+      </div>
+      {/* paths */}
+      <div className="absolute w-1/4 left-[5%] bottom-[10%]">
         <FindWay />
       </div>
-      <div className="absolute w-1/3">
+      <div className="absolute w-1/4 -z-10 left-[25%] top-[40%] -translate-y-1/2">
         <StrangerDanger />
       </div>
-      <div className="absolute w-1/3">
+      <div className="absolute w-4/12 -z-10 right-[15%] top-[40%] -translate-y-1/2">
         <BuySomething />
       </div>
-      <div className="absolute w-1/3">
+      <div className="absolute w-1/4 bottom-[10%] right-[5%]">
         <FindParents />
       </div>
     </div>
