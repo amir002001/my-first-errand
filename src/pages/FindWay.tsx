86c24f576f8617bd90fb7a4765c07a0039e999cc
Amir Azizafshari <a_amirhossein@icloud.com>
2022-11-19 22:38:40 -0500

    adds dialog and text

--- src/pages/FindWay.tsx
+++ src/pages/FindWay.tsx
@@ -1,3 +1,6 @@
+import Burhan from "../components/svgs/Characters/Burhan";
+import Dialog from "../components/svgs/FindWay/Dialog";
+import SuperMarketWithTiles from "../components/svgs/FindWay/SuperMarketWithTiles";
 import BuySomethingCloud from "../components/svgs/Paths/BuySomethingCloud";
 import FindParentsCloud from "../components/svgs/Paths/FindParentsCloud";
 import FindWayCloud from "../components/svgs/Paths/FindWayCloud";
@@ -17,7 +20,30 @@ function FindWay() {
       <div className="absolute w-1/3 opacity-[38%] right-0 top-[5%] -z-20 translate-x-1/3">
         <Cloud />
       </div>
-      
+      {/* Dialog box and text */}
+      <div className="absolute w-[80%] left-1/2 bottom-[20%] -translate-x-1/2 ">
+        <div className="relative w-full h-full flex items-center">
+          <div className="absolute w-full -z-10">
+            <Dialog />
+          </div>
+          <div className="w-[10%]"></div>
+          <h4 className="text-3xl text-[#FFE9B1] grow">
+            Hi <span>Galaxia</span>, I’m Burhan! Let’s go to the grocery store
+            to buy something today
+          </h4>
+          <button className="min-w-fit text-white bg-[#478BF0] p-2 rounded-xl self-center text-xl mr-[10vw] ">
+            Let's Go
+          </button>
+        </div>
+      </div>
+      {/* Character and Character name */}
+      <div className="hidden">
+        <div>
+          <Burhan />
+        </div>
+        <h2>Burhan</h2>
+      </div>
+      <SuperMarketWithTiles />
     </div>
   );
 }
